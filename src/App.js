c0584b037e9549cf82099e3a91da58245d0f49c8
claudio <bmx347@gmail.com>
2022-09-27 00:07:54 -0300

    second

--- src/App.js
+++ src/App.js
@@ -1,7 +1,117 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [namess, setNamess] = useState([]);
+
+  for (let i = 1; i <= 100; i++) {
+     
+      let output = ''
+      if (i % 3 === 0) {
+          output += 'Fizz'
+      }
+      if (i % 5 === 0) {
+          output += 'Buzz'
+      }
+    //  console.log(output || i)
+    
+  }
+
+  /////////////////////  
+  const obj = {  
+  id1: {
+    name: 'John',
+    age: 30,
+    city: 'New York'
+  },
+  id2: {
+    name: 'Tim',
+    age: 10,
+    city: 'New York'
+  },
+  id3: {
+    name: 'David',
+    age: 25,
+    city: 'New York'
+  }
+}
+
+ // console.log(obj);
+
+  const keys = Object.keys(obj);
+
+  //  let arr = [];
+  //  keys.map(el => {
+  //     arr.push(obj[el]);
+
+  //  }) 
+   // console.log(arr);
+   const fn = (arr) => {
+    const newArr = arr.map(el => {
+      return el.age;
+    })
+  //  console.log(newArr);
+   
+  }
+  
+  useEffect(() => {
+    let arr = [];
+    keys.map(el => {
+       arr.push(obj[el]);
+ 
+    })
+   // console.log(arr);
+    const result = arr.map(el => {
+       return el.name
+     })
+     setData(result);
+  }, [])
+
+  // console.log(data);
+
+ const miArr = [4, 15, 80, 10, 1, 8, 23]
+ const names = ['tom', 'david', 'tina', 'alexa', 'delta', 'claude', 'james', 'patrick']
+ // console.log(names);
+ 
+ const orderNames = names.sort((a, b) => {
+  if (a > b) {
+    return 1;
+  }
+  if (a < b) {
+    return -1;
+  }
+  return 0;
+  })
+ // console.log(orderNames);
+    
+  useEffect(() => {
+      setNamess(orderNames.join(', '));
+  }, [])
+  
+//////////////////// 
+ const orderArr = miArr.sort((a, b) => a - b);
+  // console.log(orderArr);
+
+//////////////////  
+
+ // console.log(new Date().toLocaleDateString());
+
+ const d = new Date();
+let day = d.getDate();
+
+// console.log(day);
+
+const month = d.getMonth() + 1;
+// console.log(month);
+ 
+
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,9 +125,23 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         <ul>
+            {data && data.map((el, index) => {
+              return <li key={index}>{el}</li>
+            })}
+         </ul>
         </a>
+        {/* <ul>
+        {namess && namess.map((el, index) => {
+          return <li key={index}>{el}</li>
+        })}
+        
+      </ul> */}
+      <p style={{textTransform: 'capitalize'}}>{namess}</p>
+       <h3 style={{margin: '0'}}>{new Date().toLocaleDateString()}</h3>
+        <h4 style={{margin: '0'}}>The month is {month}</h4>
       </header>
+
     </div>
   );
 }
